35dad10948f726ce0f36d4e0460f0bb6145b01c8
miftodii <miftodii@bmj.com>
2020-05-08 14:18:58 +0100

    assertions

--- src/test/java/BookShelfSpec.java
+++ src/test/java/BookShelfSpec.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +27,22 @@ class BookShelfSpec
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), "BookShelf should be empty.");
+    }
+
+    @Test
+    void shouldCheckForEvenNumbers() {
+        int number = new Random(10).nextInt();
+        assertTrue(() -> number%2 == 0, number+ " is not an even number.");
+
+        BiFunction<Integer, Integer, Boolean> divisible = (x, y) -> x % y == 0;
+        Function<Integer, Boolean> multipleOf2 = (x) -> divisible.apply(x, 2);
+        assertTrue(() -> multipleOf2.apply(number), () -> " 2 is not factor of " + number);
+
+        List<Integer> numbers = Arrays.asList(1, 1, 1, 1, 2);
+        assertTrue(() -> numbers.stream().distinct().anyMatch(this::isEven), "Did not find an even number in the list");
+    }
+
+    boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
