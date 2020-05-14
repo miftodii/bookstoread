cb4ec85773dcc2b68e7b4d081f24d951bee66e92
miftodii <miftodii@bmj.com>
2020-05-14 20:29:46 +0100

    Feature: Track Bookshelf Progress

--- src/main/java/bookstoread/BookShelf.java
+++ src/main/java/bookstoread/BookShelf.java
@@ -16,29 +16,43 @@ public class BookShelf
 {
     private final List<Book> books = new ArrayList<>();
 
-    public List<Book> books() {
+    public List<Book> books()
+    {
         return Collections.unmodifiableList(books);
     }
 
-    public void add(Book... booksToAdd) {
+    public void add(Book... booksToAdd)
+    {
         Arrays.stream(booksToAdd).forEach(books::add);
     }
 
-    public List<Book> arrange() {
+    public List<Book> arrange()
+    {
         return arrange(Comparator.naturalOrder());
     }
 
-    public List<Book> arrange(Comparator<Book> criteria) {
+    public List<Book> arrange(Comparator<Book> criteria)
+    {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
 
-    public Map<Year, List<Book>> groupByPublicationYear() {
+    public Map<Year, List<Book>> groupByPublicationYear()
+    {
         return groupBy(book -> Year.of(book.getPublishedOn().getYear()));
     }
 
-    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx)
+    {
         return books
                 .stream()
                 .collect(groupingBy(fx));
+    }
+
+    public Progress progress() {
+        int booksRead = Long.valueOf(books.stream().filter(Book::isRead).count()).intValue();
+        int booksToRead = books.size() - booksRead;
+        int percentageCompleted = booksRead * 100 / books.size();
+        int percentageToRead = booksToRead * 100 / books.size();
+        return new Progress(percentageCompleted, percentageToRead, 0);
     }
 }
---
+++ src/test/java/bookstoread/BookShelfProgressSpec.java
@@ -0,0 +1,57 @@
+package bookstoread;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("A bookshelf progress")
+@ExtendWith(BooksParameterResolver.class)
+public class BookShelfProgressSpec {
+    private BookShelf shelf;
+    private Book effectiveJava;
+    private Book codeComplete;
+    private Book mythicalManMonth;
+    private Book cleanCode;
+    private Book refactoring;
+
+    @BeforeEach
+    void init(Map<String, Book> books) {
+        shelf = new BookShelf();
+        effectiveJava = books.get("Effective Java");
+        cleanCode = books.get("Clean Code");
+        mythicalManMonth = books.get("The Mythical Man-Month");
+        codeComplete = books.get("Code Complete");
+        refactoring = books.get("Refactoring: Improving the Design of Existing Code");
+
+        shelf.add(effectiveJava, cleanCode, mythicalManMonth, codeComplete, refactoring);
+    }
+
+    @Test
+    @DisplayName("is 0% completed and 100% to-read when no book is read yet")
+    void progress100PercentUnread() {
+        Progress progress = shelf.progress();
+        assertThat(progress.completed()).isEqualTo(0);
+        assertThat(progress.toRead()).isEqualTo(100);
+    }
+
+    @Test
+    @DisplayName("is 40% completed and 60% to-read when 2 books are finished and 3 books not read yet")
+    void progressWithCompletedAndToReadPercentages() {
+        effectiveJava.startedReadingOn(LocalDate.of(2016, Month.JULY, 1));
+        effectiveJava.finishedReadingOn(LocalDate.of(2016, Month.JULY, 31));
+        cleanCode.startedReadingOn(LocalDate.of(2016, Month.AUGUST, 1));
+        cleanCode.finishedReadingOn(LocalDate.of(2016, Month.AUGUST, 31));
+
+        Progress progress = shelf.progress();
+
+        assertThat(progress.completed()).isEqualTo(40);
+        assertThat(progress.toRead()).isEqualTo(60);
+    }
+}
